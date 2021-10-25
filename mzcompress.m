06246f0388db08e5a914195b924357481bcc117f
Michael D. Sorochan Armstrong <45431766+mdarmstr@users.noreply.github.com>
2021-10-25 16:34:48 -0600

    mzcompress
    When you get a moment, can you apply this function to the raw practice data? It just sums the m/z ratios to unit mass. For whatever reason, the raw data reported a higher mass resolution than the instrument was capable of. Maybe it was a poor method, but somehow I doubt it. Attached is also the raw data file as it was output. You can use the mz subdirectory as the input mz in the function. Let me know if you have any questions!

---
+++ mzcompress.m
@@ -0,0 +1,18 @@
+function [mzc,xicc] = mzcompress(mz,xic)
+
+%round the mz vector
+mzs = round(mz);
+
+qq = 1;
+
+for ii = min(mzs):max(mzs)
+   
+    indx = find(mzs==ii);
+    
+    xicc(:,qq) = sum(xic(:,indx),2); %#ok
+    mzc(qq) = ii; %#ok
+    qq = qq + 1;
+    
+end
+
+end
